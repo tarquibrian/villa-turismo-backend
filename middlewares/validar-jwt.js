df62d24cde4c75765c33e4ceebe5c86efa7a7fad
tarqui <tarquibrian@gmail.com>
2022-02-17 02:09:00 -0400

    enviar datos de uid, name

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,42 +1,32 @@
-const { response } = require('express');
-const jwt = require('jsonwebtoken');
+const { response } = require("express");
+const jwt = require("jsonwebtoken");
 
-const validarJWT = ( req, res = response, next ) => {
+const validarJWT = (req, res = response, next) => {
+  // x-token headers
+  const token = req.header("x-token");
 
-    // x-token headers
-    const token = req.header('x-token');
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: "No hay token en la petición",
+    });
+  }
 
-    if ( !token ) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'No hay token en la petición'
-        });
-    }
+  try {
+    const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
 
-    try {
-        
-        const { uid, name } = jwt.verify(
-            token,
-            process.env.SECRET_JWT_SEED
-        );
+    req.uid = uid;
+    req.name = name;
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token no válido",
+    });
+  }
 
-        req.uid = uid;
-        req.name = name;
-
-
-    } catch (error) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Token no válido'
-        });
-    }
-
-
-
-    next();
-}
-
+  next();
+};
 
 module.exports = {
-    validarJWT
-}
+  validarJWT,
+};
