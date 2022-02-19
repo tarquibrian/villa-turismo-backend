e4c2d4ce39f88307d8ecf64d49acfc0e21412b24
tarqui <tarquibrian@gmail.com>
2022-02-18 23:40:59 -0400

    cambios en el index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const { dbConnection } = require('./database/config');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
 
 // Crear el servidor de express
 const app = express();
@@ -10,28 +10,22 @@ const app = express();
 dbConnection();
 
 // CORS
-app.use(cors())
+app.use(cors());
 
 // Directorio Público
-app.use( express.static('public') );
+app.use(express.static("public"));
 
 // Lectura y parseo del body
-app.use( express.json() );
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/events', require('./routes/events') );
-
-
-
-
-// Escuchar peticiones
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events", require("./routes/events"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
 });
 
-
-
-
-
-
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
