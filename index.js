84d516963bfb306c0994c4dacfda2dd8131b747f
tarqui <tarquibrian@gmail.com>
2022-03-01 22:47:20 -0400

    se agrego la tecnologia pwa

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const { dbConnection } = require("./database/config");
+const multer = require("multer");
+const path = require("path");
 
 // Crear el servidor de express
 const app = express();
@@ -17,10 +19,28 @@ app.use(express.static("public"));
 
 // Lectura y parseo del body
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+// Lectura de imagenes
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/categories", require("./routes/categories"));
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
